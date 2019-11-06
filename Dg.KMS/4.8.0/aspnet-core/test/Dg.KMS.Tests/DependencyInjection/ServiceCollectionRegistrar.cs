d476ae24a062caae609ce2729e4d0210080bc936
帕瓦罗地瓜PowerDG <1049365046@qq.com>
2019-11-06 23:20:48 +0800

    Create ServiceCollectionRegistrar.cs

---
+++ Dg.KMS/4.8.0/aspnet-core/test/Dg.KMS.Tests/DependencyInjection/ServiceCollectionRegistrar.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Castle.MicroKernel.Registration;
+using Castle.Windsor.MsDependencyInjection;
+using Abp.Dependency;
+using Dg.KMS.EntityFrameworkCore;
+using Dg.KMS.Identity;
+
+namespace Dg.KMS.Tests.DependencyInjection
+{
+    public static class ServiceCollectionRegistrar
+    {
+        public static void Register(IIocManager iocManager)
+        {
+            var services = new ServiceCollection();
+
+            IdentityRegistrar.Register(services);
+
+            services.AddEntityFrameworkInMemoryDatabase();
+
+            var serviceProvider = WindsorRegistrationHelper.CreateServiceProvider(iocManager.IocContainer, services);
+
+            var builder = new DbContextOptionsBuilder<KMSDbContext>();
+            builder.UseInMemoryDatabase(Guid.NewGuid().ToString()).UseInternalServiceProvider(serviceProvider);
+
+            iocManager.IocContainer.Register(
+                Component
+                    .For<DbContextOptions<KMSDbContext>>()
+                    .Instance(builder.Options)
+                    .LifestyleSingleton()
+            );
+        }
+    }
+}
