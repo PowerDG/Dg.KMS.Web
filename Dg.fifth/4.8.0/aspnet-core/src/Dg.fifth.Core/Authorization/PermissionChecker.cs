33befa6626f15934c779e983a0dd827dc6dc99f7
帕瓦罗地瓜PowerDG <1049365046@qq.com>
2019-11-06 23:32:14 +0800

    Create PermissionChecker.cs

---
+++ Dg.fifth/4.8.0/aspnet-core/src/Dg.fifth.Core/Authorization/PermissionChecker.cs
@@ -0,0 +1,14 @@
+﻿using Abp.Authorization;
+using Dg.fifth.Authorization.Roles;
+using Dg.fifth.Authorization.Users;
+
+namespace Dg.fifth.Authorization
+{
+    public class PermissionChecker : PermissionChecker<Role, User>
+    {
+        public PermissionChecker(UserManager userManager)
+            : base(userManager)
+        {
+        }
+    }
+}
