602b1670d674af0244b15749878922d05de05af6
帕瓦罗地瓜PowerDG <1049365046@qq.com>
2019-11-06 23:32:22 +0800

    Create fifthConsts.cs

---
+++ Dg.fifth/4.8.0/aspnet-core/src/Dg.fifth.Core/fifthConsts.cs
@@ -0,0 +1,11 @@
+﻿namespace Dg.fifth
+{
+    public class fifthConsts
+    {
+        public const string LocalizationSourceName = "fifth";
+
+        public const string ConnectionStringName = "Default";
+
+        public const bool MultiTenancyEnabled = true;
+    }
+}
