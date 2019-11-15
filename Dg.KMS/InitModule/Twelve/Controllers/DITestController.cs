15114ba79127ad7bc96e168c1bce0b444e81b855
Lucifer-PowerDg <1049365046@qq.com>
2019-11-15 23:59:30 +0800

    Update DITestController.cs

--- Dg.KMS/InitModule/Twelve/Controllers/DITestController.cs
+++ Dg.KMS/InitModule/Twelve/Controllers/DITestController.cs
@@ -9,7 +9,28 @@ namespace Twelve.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class DITestController : ControllerBase
+    public class DITestController : ApiController
     {
+        private readonly ITestService _testService;
+        private readonly ITestService2 _testService2;
+        private readonly ITestService3 _testService3;
+        public DITestController(ITestService testService, ITestService2 testService2, ITestService3 testService3)
+        {
+            _testService = testService;
+            _testService2 = testService2;
+            _testService3 = testService3;
+        }
+        public JsonResult Index([FromServices]ITestService testService11, [FromServices]ITestService2 testService22)
+        {
+            List<object> dict= new List<object>();
+            dict.Add(_testService.GetList("")) ;
+            dict.Add( _testService.MyProperty);
+            dict.Add( testService11.MyProperty);
+            dict.Add( _testService2.MyProperty);
+            dict.Add( testService22.MyProperty);
+            dict.Add( _testService3.MyProperty);
+
+            return Json(dict);
+        }
     }
 }
