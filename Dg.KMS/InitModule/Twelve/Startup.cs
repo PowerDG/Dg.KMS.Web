03cf670da68eb09e08b4124840fae15e0e79b0d3
Lucifer-PowerDg <1049365046@qq.com>
2019-11-15 23:12:39 +0800

    Update Startup.cs

--- Dg.KMS/InitModule/Twelve/Startup.cs
+++ Dg.KMS/InitModule/Twelve/Startup.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Autofac.Extras.DynamicProxy;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -20,11 +24,36 @@ namespace Twelve
         }
 
         public IConfiguration Configuration { get; }
+        public ILifetimeScope ApplicationContainer { get; private set; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        //public void ConfigureServices(IServiceCollection services)
+        //{
+
+        //    //services.AddApplicationInsightsTelemetry(Configuration);
+        //    services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        //}
+
+
+        public IServiceProvider ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            //services.AddApplicationInsightsTelemetry(Configuration);
+            services.AddMvc();
+            return RegisterAutofac(services);
+        }
+        private IServiceProvider RegisterAutofac(IServiceCollection services)
+        {
+            var builder = new ContainerBuilder();
+            builder.Populate(services);
+            var assembly = this.GetType().GetTypeInfo().Assembly;
+            builder.RegisterType<AopInterceptor>();
+            builder.RegisterAssemblyTypes(assembly)
+            .Where(type =>
+            typeof(IDependency).IsAssignableFrom(type) && !type.GetTypeInfo().IsAbstract)
+            .AsImplementedInterfaces()
+            .InstancePerLifetimeScope().EnableInterfaceInterceptors().InterceptedBy(typeof(AopInterceptor));
+            this.ApplicationContainer = builder.Build();
+            return new AutofacServiceProvider(this.ApplicationContainer);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
