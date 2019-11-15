b6049301f3aec2b274051819efc2ad050598025d
Lucifer-PowerDg <1049365046@qq.com>
2019-11-15 23:36:18 +0800

    Create TestService.cs

---
+++ Dg.KMS/InitModule/Twelve/TestService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Twelve
+{
+    public class TestService : ITestService
+    {
+        public TestService()
+        {
+            MyProperty = Guid.NewGuid();
+        }
+        public Guid MyProperty { get; set; }
+        public List<string> GetList(string a)
+        {
+            return new List<string>() { "LiLei", "ZhangSan", "LiSi" };
+        }
+    }
+    public interface ITestService
+    {
+        Guid MyProperty { get; }
+        List<string> GetList(string a);
+    }
+}
