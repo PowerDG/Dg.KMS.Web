1fc6a1360244a133fa63f3da2279a57f9884018c
Lucifer-PowerDg <1049365046@qq.com>
2019-11-16 23:52:01 +0800

    Update IDependency.cs

--- Dg.KMS/InitModule/KMS.Twelve/IDependency.cs
+++ Dg.KMS/InitModule/KMS.Twelve/IDependency.cs
@@ -5,11 +5,9 @@ using System.Threading.Tasks;
 
 namespace KMS.Twelve
 {
-    interface IDependency
-    {
-    }
-    //public class AopInterceptor
-    //{
+    /// <summary>
+    /// 依赖注入接口，IOC都要实现这个接口
+    /// </summary>
+    public interface IDependency { }
 
-    //}
 }
