24dc3b49eacb3ca9de00d24834f9818fea6f71e4
Lucifer-PowerDg <1049365046@qq.com>
2019-11-16 23:49:01 +0800

    Create UserManager.cs

---
+++ Dg.KMS/InitModule/KMS.Twelve/Domain/User/UserManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KMS.Twelve.Domain.User
+{
+    public interface IUserManager
+    {
+        void Register(string name);
+    }
+    /// <summary>
+    /// Autofac自动实现注入到IUermanager接口，程序用扫描IDependency接口用于自动注入
+    /// </summary>
+    public class UserManager : IUserManager, IDependency
+    {
+        public void Register(string name)
+        {
+
+        }
+    }
+}
