60bafdf0745e442fe22bfac7558c100eba21315d
Lucifer-PowerDg <1049365046@qq.com>
2019-11-20 00:15:21 +0800

    Update DefaultModule.cs

--- Dg.KMS/InitModule/KMS.Twelve/DefaultModule.cs
+++ Dg.KMS/InitModule/KMS.Twelve/DefaultModule.cs
@@ -2,6 +2,10 @@
 using Autofac.Extras.DynamicProxy;
 using KMS.Twelve.Controllers;
 using KMS.Twelve.Test;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Module = Autofac.Module;
 
 namespace KMS.Twelve
 {
@@ -19,6 +23,43 @@ namespace KMS.Twelve
 
             //builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
             //builder.RegisterType<InMemoryCache>().As<ICache>().InstancePerLifetimeScope();
+
+
+
+            // Register 方式指定具体类
+            builder.Register(c => new InjectionTestService()).As<IService>().InstancePerDependency();
+
+            // RegisterType 方式指定具体类
+            builder.RegisterType<InjectionTestService>().As<IService>().InstancePerDependency();
+
+            // 自动注入的方式，不需要知道具体类的名称
+
+            /* BuildManager.GetReferencedAssemblies()
+             * 程序集的集合，包含 Web.config 文件的 assemblies 元素中指定的程序集、
+             * 从 App_Code 目录中的自定义代码生成的程序集以及其他顶级文件夹中的程序集。
+             */
+
+            // 获取包含继承了IService接口类的程序集
+            var allAssemblies = Assembly.GetEntryAssembly()
+                .GetReferencedAssemblies()
+                .Select(Assembly.Load)
+                .Where(
+                    assembly =>
+                        assembly.GetTypes().FirstOrDefault(type => type.GetInterfaces().Contains(typeof(IService))) !=
+                        null
+                );
+
+            // RegisterAssemblyTypes 注册程序集
+            //.net core如何读取所有页面的程序集
+            //    https://q.cnblogs.com/q/113026/
+            var enumerable = allAssemblies as Assembly[] ?? allAssemblies.ToArray();
+            if (enumerable.Any())
+            {
+                builder.RegisterAssemblyTypes(enumerable)
+                    .Where(type => type.GetInterfaces().Contains(typeof(IService))).AsSelf().InstancePerDependency();
+            }
+
+
 
             //containerBuilder.RegisterType<AutoDIController>().PropertiesAutowired();
             builder.RegisterAssemblyTypes(typeof(AutoDIController).Assembly)
