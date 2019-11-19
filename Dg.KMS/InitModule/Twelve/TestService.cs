82e74464ab034d1ec7808bce9b5533c6f376071e
Lucifer-PowerDg <1049365046@qq.com>
2019-11-20 00:37:22 +0800

    Update TestService.cs

--- Dg.KMS/InitModule/Twelve/TestService.cs
+++ Dg.KMS/InitModule/Twelve/TestService.cs
@@ -9,47 +9,60 @@ namespace Twelve
         {
             MyProperty = Guid.NewGuid();
         }
+
         public Guid MyProperty { get; set; }
+
         public List<string> GetList(string a)
         {
             return new List<string>() { "LiLei", "ZhangSan", "LiSi" };
         }
     }
+
     public interface ITestService
     {
         Guid MyProperty { get; }
+
         List<string> GetList(string a);
     }
+
     public interface ITestService2
     {
         Guid MyProperty { get; }
+
         List<string> GetList();
     }
+
     public interface ITestService3
     {
         Guid MyProperty { get; }
+
         List<string> GetList();
     }
+
     public class TestService2 : ITestService2
     {
         public TestService2()
         {
             MyProperty = Guid.NewGuid();
         }
+
         public Guid MyProperty { get; set; }
+
         public List<string> GetList()
         {
             return new List<string>() { "LiLei", "ZhangSan", "LiSi" };
         }
     }
+
     public class TestService3 : ITestService3
     {
-
         public TestService3()
         {
             MyProperty = Guid.NewGuid();
         }
+
         public Guid MyProperty { get; set; }
+
         public List<string> GetList()
         {
             return new List<string>() { "LiLei", "ZhangSan", "LiSi" };
