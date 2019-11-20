bc2186ef4f05cf809112d1dbf840b0db27675e44
Lucifer-PowerDg <1049365046@qq.com>
2019-11-20 23:41:16 +0800

    Create IMessage.cs

---
+++ Dg.KMS/InitModule/KMS.Twelve/Message/IMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KMS.Twelve.Message
+{
+    public interface IMessage
+    {
+        decimal QueryBalance();
+        bool Send(string msg);
+        int TotalSend(DateTime? startDate, DateTime? endDate);
+    }
+}
