fd325c77801bb68ba37a402e2194cf6819be834c
Lucifer-PowerDg <1049365046@qq.com>
2019-11-20 23:42:08 +0800

    Create MessageService.cs

---
+++ Dg.KMS/InitModule/KMS.Twelve/Message/MessageService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KMS.Twelve.Message
+{
+    public class MessageService
+    {
+    }
+    //[MessagePlatform(Enums.MPlatform.A平台)]
+    public class ASendMessageService : IMessage
+    {
+        public decimal QueryBalance()
+        {
+            return 0;
+        }
+
+        public bool Send(string msg)
+        {
+            return true;
+        }
+
+        public int TotalSend(DateTime? startDate, DateTime? endDate)
+        {
+            return 100;
+        }
+    }
+}
