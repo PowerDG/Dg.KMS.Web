2669718cc59ac5dcdfac8a31cab37127fd9d62c4
Lucifer-PowerDg <1049365046@qq.com>
2019-11-22 00:25:40 +0800

    Create Program.cs

---
+++ Dg.KMS/InitModule/KMS.CMD/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KMS.CMD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
