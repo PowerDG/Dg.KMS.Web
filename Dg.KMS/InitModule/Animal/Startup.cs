6343bc09d4bc8fabb91257c4a8ee5997d74a2f9a
Lucifer-PowerDg <1049365046@qq.com>
2019-11-22 00:49:50 +0800

    Update Startup.cs

--- Dg.KMS/InitModule/Animal/Startup.cs
+++ Dg.KMS/InitModule/Animal/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Animal.Mammal;
 using Autofac;
+using Autofac.Features.Indexed;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -33,8 +34,8 @@ namespace Animal
             var builder = new ContainerBuilder();//准备容器
             builder.RegisterType<Doge>();//注册对象
             var container = builder.Build();//创建容器完毕
-            var dog = container.Resolve<Doge>();//通过IOC容器创建对象
-            dog.SayHello();
+            var doge = container.Resolve<Doge>();//通过IOC容器创建对象
+            doge.SayHello();
 
 
             builder.RegisterInstance(new Doge());//实例注入
@@ -44,13 +45,51 @@ namespace Animal
 
             builder.Register(c => new Person() { Name = "张三", Age = 20 });//Lambda表达式创建
             Person p1 = container.Resolve<Person>();
-
-
-
             builder.Register(c => new Person() { Name = "张三", Age = 20 });//Lambda表达式创建
             Person p2 = container.Resolve<Person>();
 
+            builder.RegisterGeneric(typeof(List<>));
+            List<string> list = container.Resolve<List<string>>();
+
+
+            //var builder = new ContainerBuilder();//准备容器
+            builder.RegisterType<Doge>().As<IAnimal>();//映射对象
+            //如果一个类型被多次注册,以最后注册的为准。
+            //通过使用PreserveExistingDefaults() 修饰符，可以指定某个注册为非默认值。
+            builder.RegisterType<Cat>().As<IAnimal>().PreserveExistingDefaults();//指定Cat为非默认值
+            ////var container = builder.Build();//创建容器完毕
+            //var dog = container.Resolve<IAnimal>();//通过IOC容器创建对象
+            //dog.SayHello();
+
+            builder.RegisterType<Doge>().Named<IAnimal>("doge");//映射对象
+            builder.RegisterType<Pig>().Named<IAnimal>("pig");//映射对象
+
+            var dog = container.ResolveNamed<IAnimal>("pig");//通过IOC容器创建对象
+            dog.SayHello();
+            builder.RegisterType<Doge>().Keyed<IAnimal>(AnumalType.Doge);//映射对象
+            builder.RegisterType<Pig>().Keyed<IAnimal>(AnumalType.Pig);//映射对象
+
+            var dogAble = container.ResolveKeyed<IAnimal>(AnumalType.Cat);//通过IOC容器创建对象
+            dogAble.SayHello();
+
+            //不过这种方式是不推荐使用的，因为autofac容器会被当作Service Locator使用，
+            //    推荐的做法是通过索引类型来实现，
+            //Autofac.Features.Indexed.IIndex<K, V> 是Autofac自动实现的一个关联类型。
+            //    使用IIndex<K, V> 作为参数的构造函数从基于键的服务中选择需要的实现：
+
+            var animal = container.Resolve<IIndex<AnumalType, IAnimal>>();
+            var cat = animal[AnumalType.Cat];
+            cat.SayHello();
+
+
         }
+
+
+        public enum AnumalType
+        {
+            Doge, Pig, Cat
+        }
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
