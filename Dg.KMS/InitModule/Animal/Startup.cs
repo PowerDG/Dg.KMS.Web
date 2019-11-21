5f32e3dcfcbec8087410514a8e97c0f4f225f566
Lucifer-PowerDg <1049365046@qq.com>
2019-11-22 00:38:06 +0800

    Update Startup.cs

--- Dg.KMS/InitModule/Animal/Startup.cs
+++ Dg.KMS/InitModule/Animal/Startup.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Animal.Mammal;
+using Autofac;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -25,6 +27,29 @@ namespace Animal
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+
+
+            var builder = new ContainerBuilder();//准备容器
+            builder.RegisterType<Doge>();//注册对象
+            var container = builder.Build();//创建容器完毕
+            var dog = container.Resolve<Doge>();//通过IOC容器创建对象
+            dog.SayHello();
+
+
+            builder.RegisterInstance(new Doge());//实例注入
+
+            //builder.RegisterInstance(Singleton.GetInstance()).ExternallyOwned();//将单例对象托管到IOC容器
+
+
+            builder.Register(c => new Person() { Name = "张三", Age = 20 });//Lambda表达式创建
+            Person p1 = container.Resolve<Person>();
+
+
+
+            builder.Register(c => new Person() { Name = "张三", Age = 20 });//Lambda表达式创建
+            Person p2 = container.Resolve<Person>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
