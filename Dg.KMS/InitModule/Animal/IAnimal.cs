c7012277771fb5e948c593371d91dd34170e8ca1
Lucifer-PowerDg <1049365046@qq.com>
2019-11-22 14:38:14 +0800

    Create IAnimal.cs

---
+++ Dg.KMS/InitModule/Animal/IAnimal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Animal
+{
+
+    public interface IAnimal
+    {
+        void SayHello();
+    }
+    public class Doge : IAnimal
+    {
+        public void SayHello()
+        {
+            Console.WriteLine("我是小狗，汪汪汪~");
+        }
+    }
+    public class Cat : IAnimal
+    {
+        public void SayHello()
+        {
+            Console.WriteLine("我是小猫，喵喵喵~");
+        }
+    }
+    public class Pig : IAnimal
+    {
+        public void SayHello()
+        {
+            Console.WriteLine("我是小猪，呼呼呼~");
+        }
+    }
+
+}
