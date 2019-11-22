e45f0657e56417fb9ed9a934c32e5d71a589f1b9
Lucifer-PowerDg <1049365046@qq.com>
2019-11-22 14:49:10 +0800

    Create Doge.cs

---
+++ Dg.KMS/InitModule/Animal/Mammal/Doge.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Animal.Mammal
+{
+    public class Doge
+    {
+        public void SayHello()
+        {
+            Console.WriteLine("我是小狗，汪汪汪~");
+        }
+    }
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
