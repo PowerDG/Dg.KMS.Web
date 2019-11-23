762aba4179004944f5e8a48c3ed745ba1b13489d
Lucifer-PowerDg <1049365046@qq.com>
2019-11-23 23:13:37 +0800

    Create Dog.cs

---
+++ Dg.KMS/InitModule/TypeDemo/Domain/Dog.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TypeDemo.Domain
+{
+    public interface IDog
+    {
+        /// <summary>
+        /// 品种
+        /// </summary>
+        string Breed { get; }
+
+        /// <summary>
+        /// 名称
+        /// </summary>
+        string Name { get; }
+    }
+
+    /// <summary>
+    /// 萨摩耶
+    /// </summary>
+
+    [Intercept(typeof(LogInterceptor))]
+    public class Samoyed : IDog
+    {
+        /// <summary>
+        /// 品种
+        /// </summary>
+        public string Breed
+        {
+            get
+            {
+                return "Samoyed（萨摩耶）";
+            }
+        }
+
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string Name
+        {
+            get
+            {
+                return "小黄";
+            }
+        }
+    }
+
+    /// <summary>
+    /// 藏獒
+    /// </summary>
+    public class TibetanMastiff : IDog
+    {
+        /// <summary>
+        /// 品种
+        /// </summary>
+        public string Breed
+        {
+            get
+            {
+                return "Mastiff Class（獒犬类）";
+            }
+        }
+
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string Name
+        {
+            get
+            {
+                return "小黑";
+            }
+        }
+    }
+}
