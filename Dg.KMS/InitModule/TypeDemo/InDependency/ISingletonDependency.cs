07c716c4aef530ef9df6b90038498e474d2c1c4a
Lucifer-PowerDg <1049365046@qq.com>
2019-11-24 13:37:23 +0800

    Create ISingletonDependency.cs

---
+++ Dg.KMS/InitModule/TypeDemo/InDependency/ISingletonDependency.cs
@@ -0,0 +1,44 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TypeDemo.InDependency
+{
+    /// <summary>
+    /// 单例接口
+    /// </summary>
+    public interface ISingletonDependency
+    {
+    }
+
+    /// <summary>
+    /// 所有接口的依赖接口，每次创建新实例
+    /// </summary>
+    /// <remarks>
+    /// 用于Autofac自动注册时，查找所有依赖该接口的实现。
+    /// 实现自动注册功能
+    /// </remarks>
+    public interface ITransientDependency
+    {
+    }
+    /// <summary>
+    /// 依赖注册接口
+    /// </summary>
+    public interface IDependencyRegistrar
+    {
+        /// <summary>
+        /// Register services and interfaces
+        /// </summary>
+        /// <param name="builder">Container builder</param>
+        /// <param name="config">Config</param>
+        void Register(ContainerBuilder builder, List<Type> listType);
+
+        /// <summary>
+        /// Order of this dependency registrar implementation
+        /// </summary>
+        int Order { get; }
+    }
+
+}
