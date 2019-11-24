56f4014651aafb8db9940fcb42bad4da73c4f5c0
Lucifer-PowerDg <1049365046@qq.com>
2019-11-24 15:30:25 +0800

    Create ControllerRegistrar.cs

---
+++ Dg.KMS/InitModule/TypeDemo/IocInfo/ControllerRegistrar.cs
@@ -0,0 +1,38 @@
+﻿using Autofac;
+using Autofac.Extras.DynamicProxy;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TypeDemo.Log;
+
+namespace TypeDemo.IocInfo
+{
+    public class ControllerRegistrar
+    {/// <summary>
+     /// 
+     /// </summary>
+        public int Order
+        {
+            get
+            {
+                return 0;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="listType"></param>
+        public void Register(ContainerBuilder builder, List<Type> listType)
+        {
+            builder.RegisterType(typeof(LogInterceptor));
+            //注册Controller,实现属性注入
+            var IControllerType = typeof(ControllerBase);
+            var arrControllerType = listType.Where(t => IControllerType.IsAssignableFrom(t) && t != IControllerType).ToArray();
+            builder.RegisterTypes(arrControllerType).PropertiesAutowired().EnableClassInterceptors();
+        }
+    }
+}
