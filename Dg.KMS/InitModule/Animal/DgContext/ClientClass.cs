4c9cef09b182b808ff152dc1956ad35199f08396
Lucifer-PowerDg <1049365046@qq.com>
2019-11-30 22:18:40 +0800

    Create ClientClass.cs

---
+++ Dg.KMS/InitModule/Animal/DgContext/ClientClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Animal.DgContext
+{
+    public class ClientClass
+    {
+        private IServiceClass _serviceImpl;
+
+        public void Set_ServiceImpl(IServiceClass serviceImpl)
+        {
+            this._serviceImpl = serviceImpl;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine(_serviceImpl.ServiceInfo());
+        }
+    }
+}
