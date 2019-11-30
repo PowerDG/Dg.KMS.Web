cbde7ec519e01e4e24ec0398ff5ce093508d16f4
Lucifer-PowerDg <1049365046@qq.com>
2019-11-30 22:18:47 +0800

    Create BaseServiceClass.cs

---
+++ Dg.KMS/InitModule/Animal/DgContext/BaseServiceClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Animal.DgContext
+{
+    public class BaseServiceClass
+    {
+    }
+    public class ServiceClassA : IServiceClass
+    {
+        public String ServiceInfo()
+        {
+            return "我是ServceClassA";
+        }
+    }
+    public class ServiceClassB : IServiceClass
+    {
+        public String ServiceInfo()
+        {
+            return "我是ServceClassB";
+        }
+    }
+}
