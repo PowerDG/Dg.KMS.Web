ff0d6de4aba6f8f53a522af0366f729f4685b4b5
Lucifer-PowerDg <1049365046@qq.com>
2019-11-30 22:18:33 +0800

    Create IServiceClass.cs

---
+++ Dg.KMS/InitModule/Animal/DgContext/IServiceClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Animal.DgContext
+{
+    public interface IServiceClass
+    {
+        String ServiceInfo();
+    }
+}
