285438facc9b5e5441c0ae3ce06a57923bcb9510
Lucifer-PowerDg <1049365046@qq.com>
2019-11-30 23:16:08 +0800

    Create IFactory.cs

---
+++ Dg.KMS/InitModule/Animal/DependencyLocate/IFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Animal.DependencyLocate
+{
+    public interface IFactory
+    {
+        IWindow MakeWindow();
+
+        IButton MakeButton();
+
+        ITextBox MakeTextBox();
+
+    }
+}
