ba5dc4a639234e3f5eea08182449abc1b4d9aab3
Lucifer-PowerDg <1049365046@qq.com>
2019-12-01 14:42:13 +0800

    Create WindowsFactory.cs

---
+++ Dg.KMS/InitModule/Animal/DependencyLocate/WindowsFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Animal.DependencyLocate
+{
+    public class WindowsFactory : IFactory
+    {
+        public IWindow MakeWindow()
+        {
+            return new WindowsWindow();
+        }
+
+        public IButton MakeButton()
+        {
+            return new WindowsButton();
+        }
+
+        public ITextBox MakeTextBox()
+        {
+            return new WindowsTextBox();
+        }
+    }
+}
