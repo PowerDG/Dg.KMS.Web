68455323910628a0f7e27430040f50abca920d56
Lucifer-PowerDg <1049365046@qq.com>
2019-12-01 14:34:49 +0800

    Create BaseButton.cs

---
+++ Dg.KMS/InitModule/Animal/DependencyLocate/BaseButton.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Animal.DependencyLocate
+{
+    public class BaseButton
+    {
+    }
+    public class WindowsButton : IButton
+    {
+        public String Description { get; private set; }
+
+        public WindowsButton()
+        {
+            this.Description = "Windows风格按钮";
+        }
+
+        public String ShowInfo()
+        {
+            return this.Description;
+        }
+    }
+    public class MacButton : IButton
+    {
+        public String Description { get; private set; }
+
+        public MacButton()
+        {
+            this.Description = " Mac风格按钮";
+        }
+
+        public String ShowInfo()
+        {
+            return this.Description;
+        }
+    }
+}
