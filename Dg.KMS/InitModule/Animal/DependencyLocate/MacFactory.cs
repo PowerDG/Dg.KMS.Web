4b74438b7a2611a1403b1ae026b8c22c5801df31
Lucifer-PowerDg <1049365046@qq.com>
2019-12-01 14:51:31 +0800

    Create MacFactory.cs

---
+++ Dg.KMS/InitModule/Animal/DependencyLocate/MacFactory.cs
@@ -0,0 +1,58 @@
+﻿using Animal.DependencyLocate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Animal.DependencyLocate
+{
+    public class MacFactory : IFactory
+    {
+        public IWindow MakeWindow()
+        {
+            return new MacWindow();
+        }
+
+        public IButton MakeButton()
+        {
+            return new MacButton();
+        }
+
+        public ITextBox MakeTextBox()
+        {
+            return new MacTextBox();
+        }
+    }
+
+    public class MacWindow : IWindow
+    {
+        public String Description { get; private set; }
+
+        public MacWindow()
+        {
+            this.Description = "Mac-Window";
+        }
+
+        public String ShowInfo()
+        {
+            return this.Description;
+        }
+    }
+
+
+
+    public class MacTextBox : ITextBox
+    {
+        public String Description { get; private set; }
+
+        public MacTextBox()
+        {
+            this.Description = "Mac-TextBox";
+        }
+
+        public String ShowInfo()
+        {
+            return this.Description;
+        }
+    }
+}
