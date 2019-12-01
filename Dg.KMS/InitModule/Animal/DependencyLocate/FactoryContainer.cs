2cb50ae1101236d97bea134c4917a05221f9d43a
Lucifer-PowerDg <1049365046@qq.com>
2019-12-01 14:51:38 +0800

    Create FactoryContainer.cs

---
+++ Dg.KMS/InitModule/Animal/DependencyLocate/FactoryContainer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Animal.DependencyLocate
+{
+    public class FactoryContainer
+    {
+        public static IFactory factory { get; private set; }
+
+        public  FactoryContainer()
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load("http://www.cnblogs.com/Config.xml");
+            XmlNode xmlNode = xmlDoc.ChildNodes[1].ChildNodes[0].ChildNodes[0];
+
+            if ("Windows" == xmlNode.Value)
+            {
+                factory = new WindowsFactory();
+            }
+            else if ("Mac" == xmlNode.Value)
+            {
+                factory = new MacFactory();
+            }
+            else
+            {
+                throw new Exception("Factory Init Error");
+            }
+        }
+    }
+}
