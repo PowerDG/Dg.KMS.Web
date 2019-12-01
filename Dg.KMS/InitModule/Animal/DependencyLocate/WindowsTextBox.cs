b3cc161ac5aeae3f69c8536aa02236ce0d464f2a
Lucifer-PowerDg <1049365046@qq.com>
2019-12-01 14:51:24 +0800

    Create WindowsTextBox.cs

---
+++ Dg.KMS/InitModule/Animal/DependencyLocate/WindowsTextBox.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Animal.DependencyLocate
+{
+    public class WindowsTextBox: ITextBox
+    {
+        public String Description { get; private set; }
+
+        public WindowsTextBox()
+        {
+            this.Description = "Windows风格按钮";
+        }
+
+        public String ShowInfo()
+        {
+            return this.Description;
+        }
+    }
+}
