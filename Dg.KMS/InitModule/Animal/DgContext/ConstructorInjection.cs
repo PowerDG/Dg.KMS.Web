89cbd73062c114028e970ddc169341c9d15933c8
Lucifer-PowerDg <1049365046@qq.com>
2019-12-01 14:51:14 +0800

    Create ConstructorInjection.cs

---
+++ Dg.KMS/InitModule/Animal/DgContext/ConstructorInjection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Animal.DgContext
+{
+    public class ConstructorInjection
+    {
+    }
+    public  class ClientClass
+    {
+        public IServiceClass _serviceImpl { get; set; }
+
+        public ClientClass()
+        {
+        }
+        public ClientClass(IServiceClass serviceImpl)
+        {
+            this._serviceImpl = serviceImpl;
+        }
+        public void Set_ServiceImpl(IServiceClass serviceImpl)
+        {
+            this._serviceImpl = serviceImpl;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine(_serviceImpl.ServiceInfo());
+        }
+    }
+}
