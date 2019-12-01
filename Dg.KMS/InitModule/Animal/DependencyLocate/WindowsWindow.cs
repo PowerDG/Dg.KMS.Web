96620e57ebe24924328a611d106d93ca352eec68
Lucifer-PowerDg <1049365046@qq.com>
2019-12-01 14:41:46 +0800

    Create WindowsWindow.cs

---
+++ Dg.KMS/InitModule/Animal/DependencyLocate/WindowsWindow.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Animal.DependencyLocate
+{
+    public class WindowsWindow: IWindow
+    {
+        public String Description { get; private set; }
+
+        public WindowsWindow()
+        {
+            this.Description = "Windows风格Window";
+        }
+
+        public String ShowInfo()
+        {
+            return this.Description;
+        }
+    }
+}
