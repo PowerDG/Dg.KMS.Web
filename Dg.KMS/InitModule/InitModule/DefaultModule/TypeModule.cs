eaae14b66744286cc61c961c4085a90fd050c5c0
Lucifer-PowerDg <1049365046@qq.com>
2019-12-08 22:22:30 +0800

    Update TypeModule.cs

--- Dg.KMS/InitModule/InitModule/DefaultModule/TypeModule.cs
+++ Dg.KMS/InitModule/InitModule/DefaultModule/TypeModule.cs
@@ -10,6 +10,8 @@ namespace InitModule.DefaultModule
 {
     public class TypeModule : Autofac.Module
     {
+        public object MySingleton { get; private set; }
+
         protected override void Load(ContainerBuilder builder)
         {
             #region NamingAssembly
@@ -26,12 +28,83 @@ namespace InitModule.DefaultModule
 
             #endregion
 
-            #region MyRegion
+            #region 类型创建RegisterType
             builder.RegisterType<AutoFacManager>();
             builder.RegisterType<Worker>().As<IPerson>();
+            //AutoFac能够通过反射检查一个类型,选择一个合适的构造函数,创造这个对象的实例。
+            //主要通过RegisterType<T>() 和 RegisterType(Type) 两个方法以这种方式建立。
+
+            //ContainerBuilder使用 As() 方法将Component封装成了服务使用。
+            #endregion
+
+            #region 2、实例创建
+            builder.RegisterInstance<AutoFacManager>(new AutoFacManager(new Worker()));
+
+            //单例
+
+            //提供示例的方式，还有一个功能，就是不影响系统中原有的单例：
+            //builder.RegisterInstance(MySingleton.GetInstance()).ExternallyOwned();  
+            ////将自己系统中原有的单例注册为容器托管的单例
+
+            //这种方法会确保系统中的单例实例最终转化为由容器托管的单例实例。
+            #endregion
+            #region 3、Lambda表达式创建
+            //Lambda的方式也是Autofac通过反射的方式实现
+
+   
+                builder.Register(c => new AutoFacManager(c.Resolve<IPerson>()));
+            builder.RegisterType<Worker>().As<IPerson>();
+
 
             #endregion
 
+
+            #region  4、程序集创建
+
+            //程序集的创建主要通过RegisterAssemblyTypes()方法实现，
+            //Autofac会自动在程序集中查找匹配的类型用于创建实例。
+
+            builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly()); //在当前正在运行的程序集中找
+            builder.RegisterType<Worker>().As<IPerson>();
+
+
+
+
+            #endregion
+
+
+
+            #region  4、程序集创建
+
+            //程序集的创建主要通过RegisterAssemblyTypes()方法实现，
+            //Autofac会自动在程序集中查找匹配的类型用于创建实例。
+
+            builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly()); //在当前正在运行的程序集中找
+            builder.RegisterType<Worker>().As<IPerson>();
+
+
+
+
+            #endregion
+
+
+
+
+            #region 　5、泛型注册
+
+            //泛型注册通过RegisterGeneric() 这个方法实现，在容易中可以创建出泛型的具体对象。
+
+    //泛型注册,可以通过容器返回List<T> 如:List<string>,List<int>等等
+    builder.RegisterGeneric(typeof(List<>)).As(typeof(IList<>)).InstancePerLifetimeScope();
+            using (IContainer container = builder.Build())
+            {
+                IList<string> ListString = container.Resolve<IList<string>>();
+            }
+
+
+
+
+            #endregion
 
 
         }
