2b5de7525fa758ea0936cf945bcc7693f879bc99
Lucifer-PowerDg <1049365046@qq.com>
2019-12-08 21:51:50 +0800

    Update TypeModule.cs

--- Dg.KMS/InitModule/InitModule/DefaultModule/TypeModule.cs
+++ Dg.KMS/InitModule/InitModule/DefaultModule/TypeModule.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.Loader;
 using System.Threading.Tasks;
 
 namespace InitModule.DefaultModule
@@ -10,6 +12,8 @@ namespace InitModule.DefaultModule
     {
         protected override void Load(ContainerBuilder builder)
         {
+            #region NamingAssembly
+
             //注册当前程序集中以“Ser”结尾的类,暴漏类实现的所有接口，生命周期为PerLifetimeScope
             builder.RegisterAssemblyTypes(System.Reflection.Assembly.GetExecutingAssembly())
                 .Where(t => t.Name.EndsWith("Ser"))
@@ -19,6 +23,13 @@ namespace InitModule.DefaultModule
                 .AsImplementedInterfaces().InstancePerLifetimeScope();
             //注册所有"MyApp.Repository"程序集中的类
             //builder.RegisterAssemblyTypes(GetAssembly("MyApp.Repository")).AsImplementedInterfaces();
+
+            #endregion
+
+
+
+
+
         }
 
 
