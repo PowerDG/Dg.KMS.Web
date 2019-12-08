874ca78f07e02c106a9d47f58da43dbd7b8689e5
Lucifer-PowerDg <1049365046@qq.com>
2019-12-08 17:25:28 +0800

    Create CashRebate.cs

---
+++ Dg.KMS/InitModule/InitModule/Cash/CashRebate.cs
@@ -0,0 +1,27 @@
+﻿namespace InitModule.Cash
+{
+    internal class CashRebate : BaseCash, ICash
+    {
+        private decimal rebate { get; set; }
+
+        public decimal money { get; set; }
+        public CashRebate(decimal v)
+        {
+            //this.Rebate = rebate
+            this.rebate = v;
+        }
+        //void CashRebate(decimal rebate) {
+        //}
+        //calculate(money) { return money * reabte}
+
+        public decimal calculate(decimal money)
+        {
+            return money * rebate;
+        }
+
+        //decimal ICash.calculate(decimal money)
+        //{
+        //    throw new System.NotImplementedException();
+        //}
+    }
+}
