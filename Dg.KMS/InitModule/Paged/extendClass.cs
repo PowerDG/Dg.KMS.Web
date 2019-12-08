c1598d1624d4b22e6bc355de0cbf050d69b5e5cb
Lucifer-PowerDg <1049365046@qq.com>
2019-12-08 17:24:35 +0800

    Update extendClass.cs

--- Dg.KMS/InitModule/Paged/extendClass.cs
+++ Dg.KMS/InitModule/Paged/extendClass.cs
@@ -4,21 +4,23 @@ using System.Text;
 
 namespace Paged
 {
-    public static class extendClass
-    {
-        // 比如，我要给List<gameProduct>这个泛型集合添加一个MyPrint方法。我们可以这样做，注意函数签名.
-        public static String MyPrint(this IEnumerable<object> list)
-        {
-            String rt = "";
-            IEnumerator<object> eartor = list.GetEnumerator();// 获得枚举器
-            rt += "{\n";
-            while (eartor.MoveNext())
-            {
-                rt += eartor.Current.ToString() + ",\n";
-            }
-            rt += "}";
-            return rt;
-        }
+    //public static class extendClass
+    //{
+    //    // 比如，我要给List<gameProduct>这个泛型集合添加一个MyPrint方法。我们可以这样做，注意函数签名.
+    //    //public static String MyPrint(this IEnumerable<object> list)
+    //    //{
+    //    //    String rt = "";
+    //    //    IEnumerator<object> eartor = list.GetEnumerator();// 获得枚举器
+    //    //    rt += "{\n";
+    //    //    while (eartor.MoveNext())
+    //    //    {
+    //    //        rt += eartor.Current.ToString() + ",\n";
+    //    //    }
+    //    //    rt += "}";
+    //    //    return rt;
+    //    //}
 
-    }
+    //}
+
+    //public 
 }
