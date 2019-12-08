3ce9379d3f347b7f4295d40cc8a9f3c3d276f954
Lucifer-PowerDg <1049365046@qq.com>
2019-12-08 17:25:55 +0800

    Create ICash.cs

---
+++ Dg.KMS/InitModule/InitModule/Cash/ICash.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InitModule.Cash
+{
+    interface ICash
+    {
+
+        decimal calculate(decimal money);
+    }
+}
