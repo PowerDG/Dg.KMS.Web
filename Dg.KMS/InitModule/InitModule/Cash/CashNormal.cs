27df0ca2ad5df745b39d0dbe8cd41d3cf31e1043
Lucifer-PowerDg <1049365046@qq.com>
2019-12-08 17:25:17 +0800

    Create CashNormal.cs

---
+++ Dg.KMS/InitModule/InitModule/Cash/CashNormal.cs
@@ -0,0 +1,14 @@
+﻿namespace InitModule.Cash
+{
+    public class CashNormal : BaseCash, ICash
+    {
+        public CashNormal()
+        {
+
+        }
+        public decimal calculate(decimal money)
+        {
+            return money;
+        }
+    }
+}
