1d7813bf25365ad99179fc061abc7217a2ff19a5
Lucifer-PowerDg <1049365046@qq.com>
2019-12-08 21:48:09 +0800

    Create TypeModule.cs

---
+++ Dg.KMS/InitModule/InitModule/DefaultModule/TypeModule.cs
@@ -0,0 +1,35 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InitModule.DefaultModule
+{
+    public class TypeModule : Autofac.Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            //注册当前程序集中以“Ser”结尾的类,暴漏类实现的所有接口，生命周期为PerLifetimeScope
+            builder.RegisterAssemblyTypes(System.Reflection.Assembly.GetExecutingAssembly())
+                .Where(t => t.Name.EndsWith("Ser"))
+                .AsImplementedInterfaces().InstancePerLifetimeScope();
+            builder.RegisterAssemblyTypes(System.Reflection.Assembly.GetExecutingAssembly())
+                .Where(t => t.Name.EndsWith("Repository"))
+                .AsImplementedInterfaces().InstancePerLifetimeScope();
+            //注册所有"MyApp.Repository"程序集中的类
+            //builder.RegisterAssemblyTypes(GetAssembly("MyApp.Repository")).AsImplementedInterfaces();
+        }
+
+
+        public static Assembly GetAssembly(string assemblyName)
+        {
+            var assembly = AssemblyLoadContext.Default.LoadFromAssemblyPath(AppContext.BaseDirectory + $"{assemblyName}.dll");
+            return assembly;
+        }
+
+        //————————————————
+        //版权声明：本文为CSDN博主「qq_39628933」的原创文章，遵循 CC 4.0 BY-SA 版权协议，转载请附上原文出处链接及本声明。
+        //原文链接：https://blog.csdn.net/qq_39628933/article/details/84964960
+    }
+}
