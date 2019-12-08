814f0c49478074f1573cdde096ec6fd92a834e97
Lucifer-PowerDg <1049365046@qq.com>
2019-12-08 17:23:01 +0800

    Create BaseCash.cs

---
+++ Dg.KMS/InitModule/InitModule/Cash/BaseCash.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InitModule.Cash
+{
+    public class BaseCash
+    //:ICash
+    {
+        public decimal money { get; set; }
+    }
+}
