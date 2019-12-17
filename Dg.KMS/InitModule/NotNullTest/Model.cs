1f0f819cc58fc64ca0265529a5ac9722b84dba56
Lucifer-PowerDg <1049365046@qq.com>
2019-12-17 20:18:08 +0800

    Create Model.cs

---
+++ Dg.KMS/InitModule/NotNullTest/Model.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NotNullTest
+{
+     public  class Model2
+    {
+        public int id { get; set; }
+
+        public string Remark { get; set; }
+    }
+}
