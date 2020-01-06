4a69b4a08d94f15c9a8cd50c2d49d01a65c5b6db
Lucifer-PowerDg <1049365046@qq.com>
2020-01-06 18:14:14 +0800

    Create YunSourseMySqlContext.cs

---
+++ Dg.KMS/InitModule/DgInitEFCore/Application/YunSourseMySqlContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace DgInitEFCore.Application
+{
+    public class YunSourseMySqlContext :   DbContext
+    {
+        //public DbSet<T> Set<T>() where T:Entity
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}
