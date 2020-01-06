abf89e887a8f2f130775a70a85472f3ffc4d860b
Lucifer-PowerDg <1049365046@qq.com>
2020-01-06 18:20:10 +0800

    第三方容器接管Core 的默认DI

--- Dg.KMS/InitModule/DgInitEFCore/Startup.cs
+++ Dg.KMS/InitModule/DgInitEFCore/Startup.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using DgInitEFCore.Application;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -22,13 +25,21 @@ namespace DgInitEFCore
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        //public void ConfigureServices(IServiceCollection services)
+        public IContainer ApplicationContainer { get; private set; }
+        public IServiceProvider ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-
-
-            //builder.RegisterGeneric(typeof(EfRepositoryBase<>)).As(typeof(IRepository<>)).InstancePerDependency();//注册仓储泛型
-
+            //————————————————
+            //版权声明：本文为CSDN博主「黄黄同学爱学习」的原创文章，遵循 CC 4.0 BY-SA 版权协议，转载请附上原文出处链接及本声明。
+            //原文链接：https://blog.csdn.net/huanghuangtongxue/article/details/78914306
+            services.AddMvc();//.SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+             
+            var builder = new ContainerBuilder();//实例化 AutoFac  容器            
+            builder.Populate(services);
+            builder.RegisterGeneric(typeof(EfRepositoryBase<>)).As(typeof(IRepository<>)).InstancePerDependency();//注册仓储泛型
+            ApplicationContainer = builder.Build();
+            return new AutofacServiceProvider(ApplicationContainer);//第三方IOC接管 core内置DI容器
+         
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
