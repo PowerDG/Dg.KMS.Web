510a60766841d878e6acffdef18b5f458cb0e899
Lucifer-PowerDg <1049365046@qq.com>
2020-01-06 18:34:45 +0800

    Update ShareYunSourseAppService.cs

--- Dg.KMS/InitModule/DgInitEFCore/Application/ShareYunSourseAppService.cs
+++ Dg.KMS/InitModule/DgInitEFCore/Application/ShareYunSourseAppService.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace DgInitEFCore.Application
 {
-    public class ShareYunSourseAppService     : IShareYunSourseAppService
+    public class ShareYunSourseAppService : IShareYunSourseAppService
     {
         private readonly IRepository<YunSourse> _yunsourseIRepository;
         public ShareYunSourseAppService(IRepository<YunSourse> yunsourseIRepository)
@@ -18,7 +18,7 @@ namespace DgInitEFCore.Application
 
         }
     }
-//————————————————
-//版权声明：本文为CSDN博主「黄黄同学爱学习」的原创文章，遵循 CC 4.0 BY-SA 版权协议，转载请附上原文出处链接及本声明。
-//原文链接：https://blog.csdn.net/huanghuangtongxue/article/details/78937494
+    //————————————————
+    //版权声明：本文为CSDN博主「黄黄同学爱学习」的原创文章，遵循 CC 4.0 BY-SA 版权协议，转载请附上原文出处链接及本声明。
+    //原文链接：https://blog.csdn.net/huanghuangtongxue/article/details/78937494
 }
