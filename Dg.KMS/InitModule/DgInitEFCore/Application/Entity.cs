77f002b1039bb2a6c49c49c1f12f60017672b242
Lucifer-PowerDg <1049365046@qq.com>
2020-01-06 18:14:34 +0800

    Create Entity.cs

---
+++ Dg.KMS/InitModule/DgInitEFCore/Application/Entity.cs
@@ -0,0 +1,22 @@
+﻿namespace DgInitEFCore.Application
+{
+    /// <summary>
+    /// 泛型实体基类
+    /// </summary>
+    /// <typeparam name="TPrimaryKey">主键类型</typeparam>
+    public abstract class Entity<TPrimaryKey>
+    {
+        /// <summary>
+        /// 主键
+        /// </summary>
+        public virtual TPrimaryKey Id { get; set; }
+    }
+
+    /// <summary>
+    /// 定义默认主键类型为int的实体基类
+    /// </summary>
+    public abstract class Entity : Entity<int>
+    {
+
+    }
+}
