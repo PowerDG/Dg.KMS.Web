543b8cf074e491912d597ac94c413c46b6d8686e
Lucifer-PowerDg <1049365046@qq.com>
2020-03-09 23:16:55 +0800

    Create LambdaExtension.cs

---
+++ PowerDgKMS/Dg.Extensions/LambdaExtension.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Dg.Extensions
+{
+    public static class LambdaExtension
+    {
+        #region WhereIfExtension
+
+        /// <summary>
+        ///https://www.cnblogs.com/ldp615/archive/2011/02/17/WhereIf-ExtensionMethod.html
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="predicate"></param>
+        /// <param name="condition"></param>
+        /// <returns></returns>
+        public static IQueryable<T> WhereIf<T>(this IQueryable<T> source, Expression<Func<T, bool>> predicate, bool condition)
+        {
+            return condition ? source.Where(predicate) : source;
+        }
+        public static IQueryable<T> WhereIf<T>(this IQueryable<T> source, Expression<Func<T, int, bool>> predicate, bool condition)
+        {
+            return condition ? source.Where(predicate) : source;
+        }
+        public static IEnumerable<T> WhereIf<T>(this IEnumerable<T> source, Func<T, bool> predicate, bool condition)
+        {
+            return condition ? source.Where(predicate) : source;
+        }
+        public static IEnumerable<T> WhereIf<T>(this IEnumerable<T> source, Func<T, int, bool> predicate, bool condition)
+        {
+            return condition ? source.Where(predicate) : source;
+        }
+
+        #endregion
+
+        #region IsBetween
+        public static bool IsBetween<T>(this T t, T lowerBound, T upperBound,
+    bool includeLowerBound = false, bool includeUpperBound = false)
+        where T : IComparable<T>
+        {
+            if (t == null) throw new ArgumentNullException("t");
+
+            var lowerCompareResult = t.CompareTo(lowerBound);
+            var upperCompareResult = t.CompareTo(upperBound);
+
+            return (includeLowerBound && lowerCompareResult == 0) ||
+                (includeUpperBound && upperCompareResult == 0) ||
+                (lowerCompareResult > 0 && upperCompareResult < 0);
+        }
+        #endregion
+    }
+}
