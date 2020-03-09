7f5267b5b7c584d6858e689d2ec830c39caef479
Lucifer-PowerDg <1049365046@qq.com>
2020-03-10 00:23:31 +0800

    Update LambdaExtension.cs

--- PowerDgKMS/Dg.Extensions/LambdaExtension.cs
+++ PowerDgKMS/Dg.Extensions/LambdaExtension.cs
@@ -179,19 +179,81 @@ namespace Dg.Extensions
             return result;
         }
 
+
+
+
         #endregion
 
     }
 
-
-    public class SwithCase<TCase, TOther>
+    public static class SwithCaseExtension
     {
-        public SwithCase(TCase value, Action<TOther> action)
+        public class SwithCase<TCase, TOther>
         {
-            Value = value;
-            Action = action;
+            public SwithCase(TCase value, Action<TOther> action)
+            {
+                Value = value;
+                Action = action;
+            }
+            public TCase Value { get; private set; }
+            public Action<TOther> Action { get; private set; }
         }
-        public TCase Value { get; private set; }
-        public Action<TOther> Action { get; private set; }
+
+
+
+
+
+        //}
+
+        #region Swith
+        public static SwithCase<TCase, TOther> Switch<TCase, TOther>(this TCase t, Action<TOther> action) where TCase : IEquatable<TCase>
+        {
+            return new SwithCase<TCase, TOther>(t, action);
+        }
+
+        public static SwithCase<TCase, TOther> Switch<TInput, TCase, TOther>(this TInput t, Func<TInput, TCase> selector, Action<TOther> action) where TCase : IEquatable<TCase>
+        {
+            return new SwithCase<TCase, TOther>(selector(t), action);
+        }
+        #endregion
+
+        #region Case
+        public static SwithCase<TCase, TOther> Case<TCase, TOther>(this SwithCase<TCase, TOther> sc, TCase option, TOther other) where TCase : IEquatable<TCase>
+        {
+            return Case(sc, option, other, true);
+        }
+
+
+        public static SwithCase<TCase, TOther> Case<TCase, TOther>(this SwithCase<TCase, TOther> sc, TCase option, TOther other, bool bBreak) where TCase : IEquatable<TCase>
+        {
+            return Case(sc, c => c.Equals(option), other, bBreak);
+        }
+
+
+        public static SwithCase<TCase, TOther> Case<TCase, TOther>(this SwithCase<TCase, TOther> sc, Predicate<TCase> predict, TOther other) where TCase : IEquatable<TCase>
+        {
+            return Case(sc, predict, other, true);
+        }
+
+        public static SwithCase<TCase, TOther> Case<TCase, TOther>(this SwithCase<TCase, TOther> sc, Predicate<TCase> predict, TOther other, bool bBreak) where TCase : IEquatable<TCase>
+        {
+            if (sc == null) return null;
+            if (predict(sc.Value))
+            {
+                sc.Action(other);
+                return bBreak ? null : sc;
+            }
+            else return sc;
+        }
+        #endregion
+
+        #region Default
+        public static void Default<TCase, TOther>(this SwithCase<TCase, TOther> sc, TOther other)
+        {
+            if (sc == null) return;
+            sc.Action(other);
+
+        }
+        #endregion
     }
 }
