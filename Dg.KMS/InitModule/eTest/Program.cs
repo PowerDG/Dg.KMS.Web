b22e8a60cb3c7b31210addc1b7e90cc2eb719352
Lucifer-PowerDg <1049365046@qq.com>
2020-03-28 20:00:50 +0800

    Create Program.cs

---
+++ Dg.KMS/InitModule/eTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using initEvernote;
+using System;
+using EvernoteSDK;
+using EvernoteSDK.Advanced;
+namespace eTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //(new EasyInit()).open();
+
+            ENSession.SetSharedSessionConsumerKey("wangshuxin", "500dd1466d35b2e5", "sandbox.evernote.com");
+
+            if (ENSession.SharedSession.IsAuthenticated == false)
+            {
+                ENSession.SharedSession.AuthenticateToEvernote();
+            }
+            ENNote myPlainNote = new ENNote();
+            //myPlainNote.
+            myPlainNote.Title = "My plain text note";
+            myPlainNote.Content = ENNoteContent.NoteContentWithString("Hello, world!");
+            ENNoteRef myPlainNoteRef = ENSession.SharedSession.UploadNote(myPlainNote, null);
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
