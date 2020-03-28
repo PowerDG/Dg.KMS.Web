3e2ddc5f120957d55e2aeaf35ffbc9c29de5143d
Lucifer-PowerDg <1049365046@qq.com>
2020-03-28 19:55:18 +0800

    initEN

---
+++ Dg.KMS/InitModule/initEvernote/easyInit.cs
@@ -0,0 +1,28 @@
+﻿using EvernoteSDK;
+using EvernoteSDK.Advanced;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace initEvernote
+{
+    public class EasyInit
+    {
+        public void open()
+        {
+            // Be sure to put your own consumer key and consumer secret here.
+            ENSession.SetSharedSessionConsumerKey("wangshuxin", "500dd1466d35b2e5");
+
+            //if (ENSession.SharedSession.IsAuthenticated == false)
+            //{
+            //    ENSession.SharedSession.AuthenticateToEvernote();
+            //}
+            ENNote myPlainNote = new ENNote();
+            //myPlainNote.
+            myPlainNote.Title = "My plain text note";
+            myPlainNote.Content = ENNoteContent.NoteContentWithString("Hello, world!");
+            ENNoteRef myPlainNoteRef = ENSession.SharedSession.UploadNote(myPlainNote, null);
+        }
+
+    }
+}
