36d357a51d2f024bab21a821b02a6f6b70213c28
Lucifer-PowerDg <1049365046@qq.com>
2020-04-03 08:10:24 +0800

    Update easyInit.cs

--- Dg.KMS/InitModule/initEvernote/easyInit.cs
+++ Dg.KMS/InitModule/initEvernote/easyInit.cs
@@ -52,13 +52,27 @@ namespace initEvernote
 
             Console.ReadKey();
         }
-
+        
         public static void SetNSession()
         {
             ENSession.SetSharedSessionConsumerKey("wangshuxin", "500dd1466d35b2e5");
             ENSession.SetSharedSessionDeveloperToken("S=s1:U=94d05:E=1787845f5a9:C=1712094c950:P=1cd:A=en-devtoken:V=2:H=a0cde57efad83817e20eb310c9ff5830", "https://sandbox.evernote.com/shard/s1/notestore");
 
             //ListTags();
+        }
+        /// <summary>
+        /// Developer Token
+        /// S=s72:U=f08200:E=1715fef8ca9:C=1713be30550:P=1cd:A=en-devtoken:V=2:H=c6627057b7da100be776ae41311bb39a
+        /// 
+        /// NoteStore URL:
+        /// https://app.yinxiang.com/shard/s72/notestore
+        /// </summary>
+        public static void SetMyENSessionAdvanced()
+        {
+            ENSessionAdvanced.SetSharedSessionConsumerKey("wangshuxin", "500dd1466d35b2e5");
+            ENSessionAdvanced.SetSharedSessionDeveloperToken("S=s72:U=f08200:E=1715ff2af63:C=1713be62a00:P=1cd:A=en-devtoken:V=2:H=682a7d1e4e9665fb93b6d9da2c57dafa", "https://app.yinxiang.com/shard/s72/notestore");
+
+            ListTags();
         }
 
         public static void SetENSessionAdvanced()
@@ -70,7 +84,7 @@ namespace initEvernote
         }
         public static void ListTags()
         {
-            ENSessionAdvanced.SharedSession.Startup();
+            //ENSessionAdvanced.SharedSession.Startup();
             ENNoteStoreClient store =
              //ENNoteStoreClient.NoteStoreClient("https://sandbox.evernote.com/shard/s1/notestore", "S=s1:U=94d05:E=1787845f5a9:C=1712094c950:P=1cd:A=en-devtoken:V=2:H=a0cde57efad83817e20eb310c9ff5830");
              ENSessionAdvanced.SharedSession.PrimaryNoteStore ;//.BusinessNoteStore;
@@ -84,20 +98,21 @@ namespace initEvernote
                 list.ForEach(x=>
                 {
                     //Console.WriteLine($"N{JsonConvert.SerializeObject(x)}"); 
-                    Console.WriteLine($"N{x.Name}==={x.UpdateSequenceNum}==={x.__isset}");
-                    x.Name = $"Dg{x.Name}";
-                    store.UpdateTag(x);
-                    Console.WriteLine($"N{ x.Guid}@@@{ x.ParentGuid}");
+                    Console.WriteLine($"【{x.Name}】==={x.UpdateSequenceNum}==={x.__isset}");
+                    //x.Name = $"Dg{x.Name}";
+                    //store.UpdateTag(x);
+                    Console.WriteLine($"N{ x.Guid}@@@【{ x.ParentGuid}】");
                 });
                 //store.UntagAll();
 
                  var currentTag = list.FirstOrDefault();
 
-                store.CreateTag(currentTag);
-                store.GetTag(currentTag.Guid); 
-                store.UpdateTag(currentTag); 
-                store.ExpungeTag(currentTag.Guid);
-                store.ListTagsByNotebook(currentTag.Guid );
+                Console.WriteLine("【List  FirstOrDefault】");
+                //store.CreateTag(currentTag);
+                //store.GetTag(currentTag.Guid); 
+                //store.UpdateTag(currentTag); 
+                //store.ExpungeTag(currentTag.Guid);
+                //store.ListTagsByNotebook(currentTag.Guid );
 
                 store.GetNoteTagNames(currentTag.Guid);
                 //store.UpdateTag(currentTag);
