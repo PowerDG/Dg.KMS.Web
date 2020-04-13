3c78f40a0034dab97375e5c356e3687650c6ad1c
Lucifer-PowerDg <1049365046@qq.com>
2020-04-14 00:33:05 +0800

    initAnt

--- PowerDgKMS/DgKMS.Cube/5.4.0/my-project/src/main.js
+++ PowerDgKMS/DgKMS.Cube/5.4.0/my-project/src/main.js
@@ -2,10 +2,11 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
-
+import router from './router' 
+import { DatePicker } from 'ant-design-vue';
+Vue.use(DatePicker);
 Vue.config.productionTip = false
-
+import 'ant-design-vue/dist/antd.css'; // or 'ant-design-vue/dist/antd.less'
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
