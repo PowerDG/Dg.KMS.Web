24d8225549057402c54b60f39a8375d25a07cb63
Lucifer-PowerDg <1049365046@qq.com>
2020-04-14 01:23:00 +0800

    tree

--- PowerDgKMS/DgKMS.Cube/5.4.0/my-project/src/main.js
+++ PowerDgKMS/DgKMS.Cube/5.4.0/my-project/src/main.js
@@ -1,9 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
 import router from './router' 
+import Antd from 'ant-design-vue';
 import { DatePicker } from 'ant-design-vue';
+import Button from 'ant-design-vue/lib/button';
+import 'ant-design-vue/dist/antd.css';
+import App from './App'
+
+Vue.component(Button.name, Button);
+Vue.use(Antd);
 Vue.use(DatePicker);
 Vue.config.productionTip = false
 import 'ant-design-vue/dist/antd.css'; // or 'ant-design-vue/dist/antd.less'
--- PowerDgKMS/DgKMS.Cube/5.4.0/my-project/src/router/index.js
+++ PowerDgKMS/DgKMS.Cube/5.4.0/my-project/src/router/index.js
@@ -2,14 +2,22 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 
+import draggabledTree from '@/components/draggabledTree'
+
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/2',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    
+    {
+      path: '/',
+      name: 'draggabledTree',
+      component: draggabledTree
     }
   ]
 })
